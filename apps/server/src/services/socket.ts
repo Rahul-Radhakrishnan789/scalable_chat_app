98386693998258f39b0e75b918e3a3d550ce6aa1
Your Name <razzrahul789@gmail.com>
2024-03-18 18:01:47 +0530

    first commit

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,62 @@
+import { Server } from "socket.io";
+import Redis from "ioredis";
+
+
+const pub = new Redis({
+    host: "redis-b59f5ee-razzrahul278-b349.a.aivencloud.com",
+    port: 18367,
+    username: "default",
+    password: "AVNS_iuqxW9zpFmpDKgfJRO9",
+  });
+
+const sub = new Redis({
+    host: "redis-b59f5ee-razzrahul278-b349.a.aivencloud.com",
+    port: 18367,
+    username: "default",
+    password: "AVNS_iuqxW9zpFmpDKgfJRO9",
+});
+
+class SocketService {
+  private _io: Server;
+
+  constructor() {
+    console.log("Init Socket Service...");
+    this._io = new Server({
+      cors: {
+        allowedHeaders: ["*"],
+        origin: "*",
+      },
+    });
+    sub.subscribe("MESSAGES");
+  }
+
+  public initListeners() {
+    const io = this.io;
+    console.log("Init Socket Listeners...");
+
+    io.on("connect", (socket) => {
+      console.log(`New Socket Connected`, socket.id);
+      socket.on("event:message", async ({ message }: { message: string }) => {
+        console.log("New Message Rec.", message);
+        // publish this message to redis
+        await pub.publish("MESSAGES", JSON.stringify({ message }));
+      });
+    });
+
+    sub.on("message", async (channel, message) => {
+      if (channel === "MESSAGES") {
+        console.log("new message from redis", message);
+        io.emit("message", message);
+        // await produceMessage(message);
+        // console.log("Message Produced to Kafka Broker");
+      }
+    });
+  }
+
+  get io() {
+    return this._io;
+  }
+}
+
+export default SocketService;
+
